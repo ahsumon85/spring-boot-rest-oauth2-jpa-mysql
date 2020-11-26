32a866b665d839dfebe5531fe5f5776b232906d2
Ahasan Habib <habibsumoncse@gmail.com>
2020-11-26 10:45:35 +0600

    configure as a resource service

--- spring-boot-secure-rest/src/main/java/com/ahasan/rest/ApplicationRunner.java
+++ spring-boot-secure-rest/src/main/java/com/ahasan/rest/ApplicationRunner.java
@@ -2,8 +2,10 @@ package com.ahasan.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class ApplicationRunner {
 
 	public static void main(String[] args) {
