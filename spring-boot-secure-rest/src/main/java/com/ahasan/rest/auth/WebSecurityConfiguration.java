1edc62c712ad40ca8b87035e62db5297736d9126
Ahasan Habib <habibsumoncse@gmail.com>
2020-12-05 14:22:40 +0600

    remove unnecessary code from WebSecurityConfiguration class

--- spring-boot-secure-rest/src/main/java/com/ahasan/rest/auth/WebSecurityConfiguration.java
+++ spring-boot-secure-rest/src/main/java/com/ahasan/rest/auth/WebSecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
