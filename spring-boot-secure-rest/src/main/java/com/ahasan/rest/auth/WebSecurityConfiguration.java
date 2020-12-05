b12e324802ff4bcafd770f91ca45394f5238ca95
Ahasan Habib <habibsumoncse@gmail.com>
2020-12-05 16:12:10 +0600

    change resource server name

--- spring-boot-secure-rest/src/main/java/com/ahasan/rest/auth/WebSecurityConfiguration.java
+++ spring-boot-secure-rest/src/main/java/com/ahasan/rest/auth/WebSecurityConfiguration.java
@@ -5,39 +5,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
 /**
-*
-* @author Ahasan Habib
-* @since 03 06 20
-*/
+ *
+ * @author Ahasan Habib
+ * @since 03 06 20
+ */
 
 @Configuration
+@EnableGlobalMethodSecurity( prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-    
+	@Bean
+	protected AuthenticationManager getAuthenticationManager() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-    @Bean
-    protected AuthenticationManager getAuthenticationManager() throws Exception {
-        return super.authenticationManagerBean();
-    }
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+	}
 }
